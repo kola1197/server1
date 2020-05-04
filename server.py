9b4423b917f66c2fe506819f0e2dd4864a85ac3e
zhitm <64278750+zhitm@users.noreply.github.com>
2020-05-04 03:32:56 +0300

    servv

--- server.py
+++ server.py
@@ -1,29 +1,48 @@
 import socket
-import time
+import threading
 
-host = '127.0.0.1'
-port = 9090
-clients = []
-s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-s.bind((host, port))
+class Server:
+    clients_list = []
+    last_received_message = ""
+    def __init__(self):
+        self.server_socket = None
+        self.create_server()
 
-quit = False
-print('Server started!')
+    def create_server(self):
 
-while not quit:
-    try:
-        data, addr = s.recvfrom(1024)
-        if addr not in clients:
-            clients.append(addr)
-        itsatime = time.strftime("%Y-%m-%d-%H.%M.%S", time.localtime())
-        print("[" + addr[0] + "]=[" + str(addr[1]) + "]=[" + itsatime + "]/", end="")
-        print(data.decode("utf-8"))
-        for client in clients:
-            if addr != client:
-                s.sendto(data, client)
+        self.server_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+        local_ip = '127.0.0.1'
+        local_port = 9090
+        print('server started')
+        self.server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.server_socket.bind((local_ip, local_port))
+        self.server_socket.listen(5)
+        self.receive_message_in_a_new_thread()
 
 
-    except:
-        print('Server stopped')
-        quit = True
-s.close()
+    def receive_message(self, so):
+        while True:
+            incoming_buffer = so.recv(1024)
+            if not incoming_buffer:
+                break
+            self.last_received = incoming_buffer.decode('utf-8')
+            self.broadcast_to_all_clients(so)
+
+    def broadcast_to_all_clients(self, sender_sockets):
+        for client in self.clients_list:
+            so, (ip,port) = client
+            if so not in sender_sockets:
+                so.sendall(self.last_received_message.encode('utf-8'))
+
+    def receive_message_in_a_new_thread(self):
+        while True:
+            client = so, (ip, port) = self.server_socket.accept()
+            self.add_to_clients(client)
+            t = threading.Thread(target = self.receive_message(), args = (so,))
+            t.start()
+    def add_to_clients(self, client):
+        if client not in self.clients_list:
+            self.clients_list.append(client)
+
+if __name__ == '__main__':
+    s = Server()
